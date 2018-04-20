6f76d89fb1b36da824f73e2e9d4095c32b4f2420
Patricio <ptorresmanque@gmail.com>
2018-04-20 19:02:00 -0300

    listoko

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAeVBWEQ3oIYG8ppzU9KlImBurYrTunrSQ",
+    authDomain: "riego-7bf95.firebaseapp.com",
+    databaseURL: "https://riego-7bf95.firebaseio.com",
+    projectId: "riego-7bf95",
+    storageBucket: "riego-7bf95.appspot.com",
+    messagingSenderId: "763097820586"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +27,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +40,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: Observable<any[]>;
+  humedad: Observable<any>;
+  flujo: Observable<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, afDB: AngularFireDatabase) {
+    this.items = afDB.list('valores').valueChanges();
+    this.humedad = afDB.object('Humedad').valueChanges();
+    this.flujo = afDB.object('Flujo').valueChanges();
   }
 
 }
